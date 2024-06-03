261ba5cee3261ed1981cdb3eff3d7f1cadb2fa1a
daTaHere <adamhuy123@yahoo.com>
2024-06-02 18:22:27 -0700

    completed card and laytout

--- app/src/main/java/com/example/topic_grid/MainActivity.kt
+++ app/src/main/java/com/example/topic_grid/MainActivity.kt
@@ -1,40 +1,38 @@
 package com.example.topic_grid
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Icon
-
-
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.topic_grid.data.DataSource
 import com.example.topic_grid.model.Topic
 import com.example.topic_grid.ui.theme.Topic_GridTheme
 
@@ -46,51 +44,71 @@ class MainActivity : ComponentActivity() {
             Topic_GridTheme {
                 Surface(
                     modifier = Modifier
-                        .fillMaxSize(),
+                        .fillMaxSize()
+                        .statusBarsPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TopicGrid(
+                        modifier = Modifier.padding(
+                            start = 8.dp,
+                            end = 8.dp,
+                            top = 8.dp
+                        )
+                    )
                 }
             }
         }
     }
 }
 
+// loops elements of a Topic class and displays data vertical in a fixed 2 Column grid
 @Composable
-fun TopicApp() {
-    Surface {
-
+fun TopicGrid(modifier: Modifier = Modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        items(DataSource.topics) { topic -> 
+            TopicCard(topic = topic)
+        }
     }
-
 }
 
+// Card with image and text
 @Composable
 fun TopicCard(topic: Topic ,modifier: Modifier = Modifier) {
-    Card( modifier = modifier ) {
-        Row (
-            modifier = modifier
-        ){
-            Image(
-                painter = painterResource(id = topic.imageId),
-                contentDescription = stringResource(id = topic.nameId),
-                modifier = modifier
-            )
+    Card {
+        Row {
+            Box{
+                Image(
+                    painter = painterResource(id = topic.imageId),
+                    contentDescription = stringResource(id = topic.nameId),
+                    modifier = modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
            Column(modifier = modifier) {
-               Row (modifier = modifier.padding(top = 9.dp)){
-                   Text(
-                       text = stringResource(id = topic.nameId),
-                       style = MaterialTheme.typography.bodyMedium,
-                       modifier = modifier
-                           .padding(horizontal = 16.dp)
-                   )
-               }
-               Row (modifier = modifier.padding(start = 16.dp, top = 5.dp)){
-                    Icon(
-                        imageVector = Icons.Default.AddCircle,
-                        contentDescription = "menu",
-                        modifier = modifier
-                            .size(17.dp)
-                    )
+               Text(
+                   text = stringResource(id = topic.nameId),
+                   style = MaterialTheme.typography.bodyMedium,
+                   modifier = modifier
+                       .padding(
+                           top = 16.dp,
+                           bottom = 8.dp,
+                           start = 16.dp,
+                           end = 16.dp
+                       )
+               )
+               Row (verticalAlignment = Alignment.CenterVertically){
+                 Icon(
+                     painter = painterResource(id = R.drawable.ic_grain),
+                      null,
+                     modifier = modifier.padding(start = 16.dp)
+                 )
 
                    Text(
                        text = topic.id.toString(),
@@ -112,5 +130,12 @@ fun TopicCard(topic: Topic ,modifier: Modifier = Modifier) {
 @Composable
 fun TopicCardPreview(){
 
-    TopicCard(topic = Topic(R.string.photography, 321, R.drawable.photography))
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopicCard(topic = Topic(R.string.photography, 321, R.drawable.photography))
+    }
+
 }
